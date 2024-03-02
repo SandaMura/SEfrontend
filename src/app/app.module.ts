aad83181224485eb875c4fd74e1ab240e1780788
Sanda Mura <sanda.mura@student.upt.ro>
2024-03-02 15:02:38 +0200

    initial commit frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { RouterModule } from '@angular/router';
+
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
