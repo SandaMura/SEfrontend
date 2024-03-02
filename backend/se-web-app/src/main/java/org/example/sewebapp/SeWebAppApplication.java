d7e2b5bb417bf8e4d82dd01b940de7afdc08448f
dnegurita <diana.negurita@irian.ro>
2024-03-02 14:56:31 +0200

    initial commit for backend; TODO: change from gradle to maven

---
+++ backend/se-web-app/src/main/java/org/example/sewebapp/SeWebAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.sewebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeWebAppApplication.class, args);
+    }
+
+}
---
+++ backend/se-web-app/src/test/java/org/example/sewebapp/SeWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sewebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
