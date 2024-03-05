fdecb89408cdf3b04098a48f54e259b22d1b69f0
Sanda Mura <sanda.mura@student.upt.ro>
2024-03-05 21:38:04 +0200

    Added domain

---
+++ libs/domain/src/lib/domain.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-domain',
+  template: `
+    <p>
+      domain works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class DomainComponent {
+
+}
---
+++ libs/domain/src/lib/domain.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { DomainComponent } from './domain.component';
+
+
+
+@NgModule({
+  declarations: [
+    DomainComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    DomainComponent
+  ]
+})
+export class DomainModule { }
